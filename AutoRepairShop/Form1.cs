74d97ac1aa1b92af24b30d3cd39bfd389f4ef834
Felipe Woods <wof8317@gmail.com>
2021-12-15 11:17:45 -0600

    Added more License Headers

--- AutoRepairShop/Form1.cs
+++ AutoRepairShop/Form1.cs
@@ -18,6 +18,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 ===========================================================================
 */
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
